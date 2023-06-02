74476f3daa4f6cc630345181ae59edb34286343b
huudao2004zz <111514495+huudao2004zz@users.noreply.github.com>
2023-06-02 17:25:22 +0700

    Add files via upload

---
+++ myproject/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            my project
+        </title>
+    </head>
+    <body>
+        <h1>  
+         <?php
+          echo (rand(0 , 100))
+        ?>
+        </h1>
+      
+    </body>
+</html>
---
+++ myproject/phpexample/string.php
@@ -0,0 +1,8 @@
+<?php 
+$x = "Hello word";
+$y = "Hello word";
+
+echo $x;
+echo "<br>";
+echo $y;
+?>
